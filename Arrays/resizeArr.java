5929716b8a59d356783efcf2cd279e3dc883832b
callmeniyu <getniyashere@gmail.com>
2024-12-05 20:18:52 +0530

    add resizeArr

---
+++ Arrays/resizeArr.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+/**
+ * resizeArr
+ */
+public class resizeArr {
+    public static int[] resizeArray(int[] arr, int capacity) {
+        for (int i = 0; i < arr.length; i++) {
+            
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4 };
+        System.out.println(Arrays.toString(resizeArray(arr, 8)));
+    }
+}
